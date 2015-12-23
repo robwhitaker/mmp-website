437d423c296d7917133bd04c341841cecb7c5c30
larouxn <larouxn@gmail.com>
2015-12-23 18:15:09 -0500

    Update configs to use two separate Elm files and remove some boilerplate

--- brunch-config.js
+++ brunch-config.js
@@ -2,36 +2,13 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
-
-      // To use a separate vendor.js bundle, specify two files path
-      // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
-      // joinTo: {
-      //  "js/app.js": /^(web\/static\/js)/,
-      //  "js/vendor.js": /^(web\/static\/vendor)|(deps)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // https://github.com/brunch/brunch/tree/master/docs#concatenation
-      // order: {
-      //   before: [
-      //     "web/static/vendor/js/jquery-2.1.1.js",
-      //     "web/static/vendor/js/bootstrap.min.js"
-      //   ]
-      // }
-    },
-    stylesheets: {
-      joinTo: "css/app.css"
-    },
-    templates: {
-      joinTo: "js/app.js"
+      joinTo: {
+        "js/mmp.js": /^(web\/static\/build\/editor.js)/,
+        "js/ding.js": /^(web\/static\/build\/reader.js)/
+      }
     }
   },
-
   conventions: {
-    // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/web/static/assets". Files in this directory
-    // will be copied to `paths.public`, which is "priv/static" by default.
     assets: /^(web\/static\/assets)/
   },
 
@@ -43,7 +20,8 @@ exports.config = {
       "deps/phoenix_html/web/static",
       "web/static",
       "test/static",
-      "web/elm/Mmp.elm"
+      "web/elm/Editor.elm",
+      "web/elm/Reader.elm"
     ],
 
     // Where to compile files to
@@ -54,18 +32,19 @@ exports.config = {
   plugins: {
     elmBrunch: {
       elmFolder: 'web/elm',
-      mainModules: ['Mmp.elm'],
-      outputFolder: '../static/vendor'
+      mainModules: ['Editor.elm', 'Reader.elm'],
+      outputFolder: '../static/build'
     },
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/web\/static\/vendor/]
+      ignore: [/web\/static\/build/]
     }
   },
 
   modules: {
     autoRequire: {
-      "js/app.js": ["web/static/js/app"]
+      "js/editor.js": ["web/static/build/editor"],
+      "js/reader.js": ["web/static/build/reader"]
     }
   },
 
