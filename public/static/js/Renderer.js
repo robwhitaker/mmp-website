robwhitaker/mmp-website

commit 88c0c79c69315ec132c442e8deac9e2841056437

Setting selected id now updates reflowCheckpointId as well, allowing proper reflow after heading/dropdown navigation. Fixes #18.

public/static/js/Renderer.js
@@ -36,6 +36,7 @@ var Renderer = window.Renderer = (function() {
 
     function setSelectedId(sId) {
         selectedId = sId;
+        reflowCheckpointId = sId;
     }
 
     function goToPage(pageNum) {

commit 9741a4f85d3f5cd8e66d809a57e78ccdc28ae68e

Fixed bugs introduced in 88c0c79c. Sleep is important.

public/static/js/Renderer.js
@@ -36,7 +36,8 @@ var Renderer = window.Renderer = (function() {
 
     function setSelectedId(sId) {
         selectedId = sId;
-        reflowCheckpointId = sId;
+        updateReflowCheckpointId();
+        console.log("set (selected,reflow): (", selectedId,",",reflowCheckpointId,")");
     }
 
     function goToPage(pageNum) {