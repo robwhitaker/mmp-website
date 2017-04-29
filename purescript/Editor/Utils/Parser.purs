3268f33c56cdf5ccdf2f76243c1406bf48195c69
Rob Whitaker <robjameswhitaker@gmail.com>
2017-04-29 02:19:44 -0400

    Chapter parse errors now represented as ADT.

--- purescript/Editor/Utils/Parser.purs
+++ purescript/Editor/Utils/Parser.purs
@@ -10,8 +10,11 @@ import Control.MonadPlus (guard)
 import Control.Plus ((<|>))
 import Data.Array (all, head, length, range, tail, zip, (!!))
 import Data.Either (Either(..), either)
+import Data.Generic (class Generic, gEq, gShow)
 import Data.Int (fromString)
+import Data.List.Lazy.Types (NonEmptyList(..))
 import Data.Maybe (Maybe(..), fromMaybe, maybe)
+import Data.Monoid (class Monoid, class Semigroup, mempty)
 import Data.Newtype (unwrap)
 import Data.String (joinWith)
 import Data.String.Regex (Regex, match, regex, replace)
@@ -22,16 +25,18 @@ import Data.Tuple (Tuple(..))
 import Editor.Models.Chapter (Chapter(..))
 import Editor.Models.Entry (Entry(..))
 
-parseChapter :: String -> Except String Chapter
+type ChapterParser = Except (Array ChapterParseError)
+
+parseChapter :: String -> ChapterParser Chapter
 parseChapter srcStr = do
     stylesheet <- getTagContents "style" srcStr <|> pure ""
     headingGroups <- getHeadingGroups srcStr
-    chapterData <- maybe (throwError "parseChapter: headingGroups list empty") pure $ head headingGroups
-    entryDataArray <- maybe (throwError "parseChapter: headingGroups list empty") pure $ tail headingGroups
-    withExcept (const "parseChapter: bad chapter heading level") $ guard (runExcept (getHeadingLevel chapterData.heading) == Right 0) :: Except String Unit
+    chapterData <- onError NoHeadingGroups $ head headingGroups
+    entryDataArray <- onError NoHeadingGroups $ tail headingGroups
+    withExcept (const $ pure BadChapterHeadingLevel) $ guard (runExcept (getHeadingLevel chapterData.heading) == Right 0) :: ChapterParser Unit
     entries <- for (zip entryDataArray $ range 0 (length entryDataArray)) \(Tuple { heading, content } i) -> do
         level <- getHeadingLevel heading
-        withExcept (const $ "parseChapter: bad entry heading level on entry " <> show i) $ guard (level > 0) :: Except String Unit
+        withExcept (const $ pure BadEntryHeadingLevel) $ guard (level > 0) :: ChapterParser Unit
         pure $ Entry $ (unwrap Entry.empty) 
             { level = level
             , order = i
@@ -45,41 +50,56 @@ parseChapter srcStr = do
         , entries = entries
         }
 
-gi :: RegexFlags
-gi = global <> ignoreCase
-
 stripTags :: String -> String
 stripTags = replace tagRegex ""
   where
-    tagRegex = unsafeRegex "<.*?>|</.*?>" gi
+    tagRegex = unsafeRegex "<.*?>|</.*?>" (global <> ignoreCase)
 
-getTagContents :: String -> String -> Except String String
+getTagContents :: String -> String -> ChapterParser String
 getTagContents tagName srcStr = do
-    reg <- except eitherRegex
-    maybe (throwError $ "getTagContents: failed match for " <> tagName) pure do
+    reg <- withExcept (pure <<< RegexError) $ except eitherRegex
+    onError (FailedTagMatch tagName) do
         matches <- match reg srcStr
         join $ matches !! 1
   where
     eitherRegex = regex (joinWith "" ["<", tagName, ".*?>((.|\\n)*?)</", tagName, ">"]) ignoreCase
 
-getHeadingGroups :: String -> Except String (Array { heading :: String, content :: String })
+getHeadingGroups :: String -> ChapterParser (Array { heading :: String, content :: String })
 getHeadingGroups srcStr = do
-    matches <- maybe (throwError "getHeadingGroups: failed to match heading blocks") pure $ 
-                     join $ map sequence $ match headingBlockRegex srcStr
-    for matches \headingBlock -> maybe (throwError "getHeadingGroups: bad split") pure do
+    matches <- onError FailedToMatchHeadingGroups $ join $ map sequence $ match headingBlockRegex srcStr
+    for matches \headingBlock -> onError BadHeadingGroupSplit do
         innerMatches <- match splitContentRegex headingBlock
         heading <- join $ innerMatches !! 1
         content <- join (innerMatches !! 2) <|> pure ""
         pure { heading : heading, content : content }
   where
-    headingBlockRegex = unsafeRegex "<h[1-6].*?>.*?<\\/h[1-6]>(.|\\n)*?(?=<h[1-6].*?>|<\\/body>)" gi
+    headingBlockRegex = unsafeRegex "<h[1-6].*?>.*?<\\/h[1-6]>(.|\\n)*?(?=<h[1-6].*?>|<\\/body>)" (global <> ignoreCase)
     splitContentRegex = unsafeRegex "(<h[1-6].*?>.*?<\\/h[1-6]>)((.|\\n)*)" ignoreCase
 
-getHeadingLevel :: String -> Except String Int
-getHeadingLevel srcStr = maybe (throwError "getHeadingLevel: failed to match valid heading tag") pure do
+getHeadingLevel :: String -> ChapterParser Int
+getHeadingLevel srcStr = onError FailedHeadingLevelMatch do
     headingMatch <- join $ map sequence $ match headingRegex srcStr
     headingNum <- headingMatch !! 1
     n <- fromString headingNum
     pure $ n - 1
   where
     headingRegex = unsafeRegex "<h([1-6]).*?>" ignoreCase
+
+onError :: forall a. ChapterParseError -> Maybe a -> ChapterParser a
+onError err = maybe (throwError $ pure err) pure
+
+data ChapterParseError
+    = BadEntryHeadingLevel
+    | BadChapterHeadingLevel
+    | NoHeadingGroups
+    | FailedTagMatch String
+    | FailedToMatchHeadingGroups
+    | BadHeadingGroupSplit
+    | FailedHeadingLevelMatch
+    | RegexError String
+    | Nil
+
+derive instance genericChapterParseError :: Generic ChapterParseError
+derive instance eqChapterParseError :: Eq ChapterParseError
+instance showChapterParseError :: Show ChapterParseError where
+    show = gShow
